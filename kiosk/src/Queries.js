840377482110c15acda44b27ad84da371f4f7083
jungstar <50983731+wjd7440@users.noreply.github.com>
2020-08-18 11:25:47 +0900

    jjh_20200818 11:25

--- kiosk/src/Queries.js
+++ kiosk/src/Queries.js
@@ -5,8 +5,8 @@ export const BUS_STATION_LIST_QUERY = gql`
   query KioskBusStationList {
     KioskBusStationList {
       busStations {
-        BUSSTOP_NM
         BUS_NODE_ID
+        BUSSTOP_NM
       }
       count
     }
