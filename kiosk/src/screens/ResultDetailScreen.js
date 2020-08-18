98d363783b680179851a08e7e008d3cb267e1258
wise3007 <50983929+wise3007@users.noreply.github.com>
2020-08-18 11:13:09 +0900

    lch_20200818 11:13

--- kiosk/src/screens/ResultDetailScreen.js
+++ kiosk/src/screens/ResultDetailScreen.js
@@ -8,6 +8,11 @@ import {
 } from "react-native";
 import { useQuery } from "react-apollo-hooks";
 import { BUS_INFO_QUERY } from "../Queries";
+import Table from "@material-ui/core/Table";
+import TableHead from "@material-ui/core/TableHead";
+import TableBody from "@material-ui/core/TableBody";
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
 
 export default ({ CAR_REG_NO, ROUTE_NO, STATUS_POS, EXTIME_MIN }) => {
   const { data, loading } = useQuery(BUS_INFO_QUERY, {
@@ -26,15 +31,17 @@ export default ({ CAR_REG_NO, ROUTE_NO, STATUS_POS, EXTIME_MIN }) => {
     if (BUS_TYPE == 2) {
       return (
         <>
-          <Text numberOfLines={1} size={18} color={"#222"}>
-            버스 번호 : {ROUTE_NO}번
-          </Text>
-          <Text numberOfLines={1} size={18} color={"#222"}>
-            잔여 정류장 수 : {STATUS_POS}개
-          </Text>
-          <Text numberOfLines={1} size={18} color={"#222"}>
-            도착 예정 시간 : {EXTIME_MIN}분
-          </Text>
+          <div style={styles.bitWrap}>
+            <Text numberOfLines={1} size={18} color={"#222"}>
+              버스 번호 : {ROUTE_NO}번
+            </Text>
+            <Text numberOfLines={1} size={18} color={"#222"}>
+              잔여 정류장 수 : {STATUS_POS}개
+            </Text>
+            <Text numberOfLines={1} size={18} color={"#222"}>
+              도착 예정 시간 : {EXTIME_MIN}분
+            </Text>
+          </div>
         </>
       );
     } else {
